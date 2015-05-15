3086c7b87695f8179767bd69f7c1c53915a4a299
Andriy Leshko <andriyleshko@mail.ru>
2015-05-15 10:26:41 +0300

    main controler && main page

---
+++ protected/views/main/index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<style>
+
+@import url(css/style.css);
+
+</style>
+</head>
+<body>
+<div class="wrapper">
+
+
+<div id="main-page">
+	<h1>Аренда квартир и комнат <br>напрямую у владельцев</h1>
+	<p class="subheader">база собственников жилой недвижимости</p>
+	<div id="statistics">
+		<p>Сейчас в базе <strong>10547</strong> объектов.</p>
+		<p><a href="">1-комнатные квартиры,</a> <a href="">комнаты</a> <span class="pricerange">8.000–30.000 ₽</span></p>
+		<p><a href="">2-комнатные квартиры</a> <span class="pricerange">16.000–90.000 ₽</span></p>
+		<p><a href="">3-комнатные</a> <span class="pricerange">8.000–30.000 ₽</span></p>
+	</div>
+	<p class="disclaimer">Все объявления проверяются вручную.<br/> Информация актуальна в день публикации.</p>
+	<h2>Поделитесь с друзьями!</h2>
+	
+	<!-- VK Widget -->
+	<script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
+	<div id="vk_groups"></div>
+	<script type="text/javascript">
+	VK.Widgets.Group("vk_groups", {mode: 0, width: "620", height: "270", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 55603717);
+	</script>
+</div>
+
+
+
+</div>
+</body>
+</html>
