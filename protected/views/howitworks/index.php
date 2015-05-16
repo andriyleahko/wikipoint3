545b594b273010f4a47422d19fbed12f8f14c826
Leshko Roman <leshkoroman@gmail.com>
2015-05-16 09:42:25 +0300

    contact and how it works

---
+++ protected/views/howitworks/index.php
@@ -0,0 +1,57 @@
+<style>
+@import url(reset.css);
+@import url(style.css);
+
+body {
+	font: 18px/30px "PT Sans";
+	background: url(bg.png) repeat-y center top;
+}
+
+h1 {
+	font: 700 48px/60px "PT Sans";
+	color: #4579A6;
+	text-align: center;
+}
+
+.subheader {
+	font: italic 29px/60px "PT Sans";
+	color: #4A4A4A;
+	text-align: center;
+	margin-bottom: 60px;
+}
+
+.wrapper {
+	width: 940px;
+	background: rgba(0, 255, 255, 0.62);
+	margin: auto;
+}
+
+
+#search-box {
+	height: 180px;
+	background: #FBFAF6;
+	box-shadow: 0px 1px 4px 0px rgba(0,0,0,0.20);
+	margin-bottom: 30px;
+}
+
+
+#how-it-works {}
+
+#how-it-works li {width: 640px;	background: url() no-repeat 80px top; padding-left: 220px; margin-bottom: 30px;}
+#how-it-works li::first-line {font: 700 24px/60px "PT Sans"; display: block;}
+
+#how-it-works #one {background-image: url(img/icon-how-it-works-1.png);}
+#how-it-works #two {background-image: url(img/icon-how-it-works-2.png);}
+#how-it-works #three {background-image: url(img/icon-how-it-works-3.png);}
+#how-it-works #four {background-image: url(img/icon-how-it-works-4.png);}
+
+
+</style>
+<h1>Как работает “БАЗА 812”</h1>
+<p class="subheader">секреты качественных объявлений</p>
+<ul id="how-it-works">
+	<li id="one">1. Сбор объявлений <br/>Мы находим новые объявления о сдаваемой недвижимости, с помощью социальных сетей, досок объявлений и базличных баз жилья. Так же, не мало объектов добавляют и сами пользователи.</li>
+	<li id="two">2. Автоматическая проверка <br/>Все новые объявления сверяются со списком агентов и нечестных арендодателей, и таким образом автоматически очищаются.</li>
+	<li id="three">3. Ручная проверка <br/>Наши модераторы звонят по каждому объявлению и уточняют все данные.<br/> После этого, можно с уверенностью сказать, что все объявления в Базе 812 от собственников и содержат актуальную информацию.</li>
+	<li id="four">4. Размещение объявлений <br/>Публикуем объявления,  делаем email-рассылки и смс-рассылки нашим клиентам.</li>
+</ul>
