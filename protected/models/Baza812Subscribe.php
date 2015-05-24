bb615c5d99ca58b456ad2c93df0989284565610c
Leshko Roman <leshkoroman@gmail.com>
2015-05-24 12:10:42 +0300

    subscribe

---
+++ protected/models/Baza812Subscribe.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * This is the model class for table "baza812_subscribe".
+ *
+ * The followings are the available columns in table 'baza812_subscribe':
+ * @property integer $id
+ * @property string $subscriber_name
+ * @property string $subscriber_email
+ * @property string $variant
+ * @property string $rooms_amount
+ * @property integer $price_max
+ * @property string $metro
+ * @property string $subscriber_phone
+ * @property string $people
+ * @property integer $animals
+ * @property integer $kids
+ * @property string $about_me
+ * @property string $param
+ */
+class Baza812Subscribe extends CActiveRecord
+{
+	public $verifyCode;
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'baza812_subscribe';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('subscriber_name, subscriber_email, rooms_amount, price_max, subscriber_phone, animals, kids', 'required'), // є ще поле param
+			array('subscriber_email', 'email'),
+			array('subscriber_email, subscriber_phone', 'unique'),
+			array('price_max, animals, kids', 'numerical', 'integerOnly'=>true),
+			array('subscriber_name, subscriber_email, variant, rooms_amount, metro, subscriber_phone, people', 'length', 'max'=>50),
+			array('about_me', 'length', 'max'=>250), // є ще поле param
+			array('animals, kids', 'in','range'=>array('1','2'),'allowEmpty'=>false),
+			//***************** перевірити
+			//array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
+			//*******************	
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, subscriber_name, subscriber_email, variant, rooms_amount, price_max, metro, subscriber_phone, people, animals, kids, about_me', 'safe', 'on'=>'search'), // є ще поле param
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'subscriber_name' => 'Ваше имя',
+			'subscriber_email' => 'Электронная почта',
+			'variant' => 'Вариант',
+			'rooms_amount' => 'Комнаты',
+			'price_max' => 'Максимальная цена',
+			'metro' => 'Метро',
+			'subscriber_phone' => 'Телефон',
+			'people' => 'Кто будет проживать',
+			'animals' => 'Домашние животные',
+			'kids' => 'Дети до 7лет',
+			'about_me' => 'Расскажите о себе',
+			'param' => 'Param',
+			'verifyCode' => 'Код проверки',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('subscriber_name',$this->subscriber_name,true);
+		$criteria->compare('subscriber_email',$this->subscriber_email,true);
+		$criteria->compare('variant',$this->variant,true);
+		$criteria->compare('rooms_amount',$this->rooms_amount,true);
+		$criteria->compare('price_max',$this->price_max);
+		$criteria->compare('metro',$this->metro,true);
+		$criteria->compare('subscriber_phone',$this->subscriber_phone,true);
+		$criteria->compare('people',$this->people,true);
+		$criteria->compare('animals',$this->animals);
+		$criteria->compare('kids',$this->kids);
+		$criteria->compare('about_me',$this->about_me,true);
+		$criteria->compare('param',$this->param,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Baza812Subscribe the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/views/subscribe/index.php
@@ -0,0 +1,144 @@
+<div id="subscribe">
+
+<h1>Подписка на рассылку<br/> новых объектов</h1>
+
+
+<p class="subheader">под ваши запросы</p>
+
+
+
+<?php echo CHtml::beginForm();?>
+<?php echo CHtml::errorSummary($model,null,null,array('style'=>'color:red'));?>
+<p>
+	<label class="wide">
+		<span style="float: left;">Что вы ищете?</span>
+		<fieldset class="what-property">
+			<input type="radio" name="rooms-amount[]" value="7,8,9,10,11,12,13,14,15" id="ra1" <?php echo(!isset($model->rooms_amount)||$model->rooms_amount=='7,8,9,10,11,12,13,14,15')?'checked':'';?>>
+			<label for="ra1"><span>комнату</span></label>
+			
+			<input type="radio" value="1" name="rooms-amount[]" id="ra2" <?php echo($model->rooms_amount=='1')?'checked':'';?>>
+			<label for="ra2"><span>1 комн.</span></label>
+			
+			<input type="radio" value="2" name="rooms-amount[]" id="ra3" <?php echo($model->rooms_amount=='2')?'checked':'';?>>
+			<label for="ra3"><span>2 комн.</span></label>
+		
+			<input type="radio" value="3" name="rooms-amount[]"  id="ra4" <?php echo($model->rooms_amount=='3')?'checked':'';?>>
+			<label for="ra4"><span>3 комн.</span></label>
+		
+			<input type="radio" value="4,5,6" name="rooms-amount[]" id="ra5" <?php echo($model->rooms_amount=='4,5,6')?'checked':'';?>>
+			<label for="ra5"><span>4 и более комн.</span></label>
+		</fieldset>
+</p>
+
+
+
+
+<p>
+	<label class="wide">
+		<span>Рассчитываю снять на сумму до:</span>
+		<select name="price_max">
+			<option value="15000" <?php echo($model->price_max==15000)?'selected':'';?>>15 000</option>
+			<option value="20000" <?php echo(!isset($model->price_max)||$model->price_max==20000)?'selected':'';?>>20 000</option>
+			<option value="25000" <?php echo($model->price_max==25000)?'selected':'';?>>25 000</option>
+			<option value="30000" <?php echo($model->price_max==30000)?'selected':'';?>>30 000</option>
+			<option value="35000" <?php echo($model->price_max==35000)?'selected':'';?>>35 000</option>
+			<option value="40000" <?php echo($model->price_max==40000)?'selected':'';?>>40 000</option>
+			<option value="45000" <?php echo($model->price_max==45000)?'selected':'';?>>45 000</option>
+			<option value="50000" <?php echo($model->price_max==50000)?'selected':'';?>>50 000</option>
+		</select>
+	</label class="wide">
+</p>
+
+
+<p>
+	<label class="wide">
+	<span>Подходящие станции метро:</span>
+	<a class="select-metro choose-metro">Выбрать станции</a>
+	<input type="hidden" name="metro" value="<?php echo(isset($metroName)&&$metroName)?$metroName:'';?>">
+	<span class="extra-span"></span>
+</p>
+
+
+
+<p class="break-line">&nbsp;</p>
+
+
+<p><label class="wide"><span>Ваше имя*:</span><input required type="text" name='subscriber_name' value="<?php echo $model->subscriber_name;?>"></label></p>
+<p><label class="wide"><span>Телефон*:</span><input required type="text" name='subscriber_phone' value="<?php echo $model->subscriber_phone;?>"></label><span class="extra-span"></span></p>
+<p><label class="wide"><span>Электронная почта*:</span><input required  type="text"  name='subscriber_email' value="<?php echo $model->subscriber_email;?>"></label></p>
+
+
+
+
+<p>
+	<label class="wide">
+		<span>Кто будет проживать:</span>
+		<select name="people">
+			<option value="" selected>Выберите</option>
+			<option value="Семейная пара" <?php echo($model->people=='Семейная пара')?'selected':'';?>>Семейная пара</option>
+			<option value="Одна девушка" <?php echo($model->people=='Одна девушка')?'selected':'';?>>Одна девушка</option>
+			<option value="Один мужчина" <?php echo($model->people=='Один мужчина')?'selected':'';?>>Один мужчина</option>
+			<option value="Две девушки" <?php echo($model->people=='Две девушки')?'selected':'';?>>Две девушки</option>
+			<option value="Двое мужчин" <?php echo($model->people=='Двое мужчин')?'selected':'';?>>Двое мужчин</option>
+			<option value="Две семейные пары" <?php echo($model->people=='Две семейные пары')?'selected':'';?>>Две семейные пары</option>
+			<option value="Семейная пара и сосед(ка)" <?php echo($model->people=='Семейная пара и сосед(ка)')?'selected':'';?>>Семейная пара и сосед(ка)</option>
+			<option value="другое" <?php echo($model->people=='другое')?'selected':'';?>>другое</option>
+		</select>	
+	</label>
+</p>
+
+
+
+
+<p>
+	<label class="wide">
+		<span style="float: left;">Домашние животные:</span>
+		<fieldset class="what-property">
+			<input type="radio" name="animals" id="animals-no" value=2 <?php echo(!isset($model->animals)||$model->animals=='2')?'checked':'';?>>
+			<label for="animals-no"><span>нет</span></label>
+			
+			<input type="radio" name="animals" id="animals-yes" value=1 <?php echo($model->animals=='1')?'checked':'';?>>
+			<label for="animals-yes"><span>есть</span></label>
+		</fieldset>
+</p>
+
+
+
+
+<p>
+	<label class="wide">
+		<span style="float: left;">Дети до 7лет:</span>
+		<fieldset class="what-property">
+			<input type="radio" name="kids" id="kids-no" value=2 <?php echo(!isset($model->kids)||$model->kids=='2')?'checked':'';?>>
+			<label for="kids-no"><span>нет</span></label>
+			
+			<input type="radio" name="kids" id="kids-yes" value=1 <?php echo($model->kids=='1')?'checked':'';?>>
+			<label for="kids-yes"><span>есть</span></label>
+		</fieldset>
+</p>
+
+
+
+<p><label class="wide"><span>Расскажите о себе:</span><textarea name='about_me'><?php echo (isset($model->about_me)&&$model->about_me)?$model->about_me:'';?></textarea></p>
+<p><label class="wide"><span>Присылать варианты за 50%:</span><input type="text" name='variant' value="<?php echo (isset($model->variant)&&$model->variant)?$model->variant:'';?>"></label></p>
+
+
+<p class="break-line">&nbsp;</p>
+
+<?php // $this->widget('CCaptcha'); ?> <br>
+<?php //echo CHtml::activeTextField($model,'verifyCode'); ?>
+
+
+
+<p>
+<?php echo CHtml::submitButton('Подписаться на рассылку');?>
+</p>
+
+
+<?php echo CHtml::hiddenField('subscribe', '1');?>
+<!--  <input type="hidden" name="subscribe" value="1"> -->
+<?php echo CHtml::endForm();?>
+
+
+
+</div>
